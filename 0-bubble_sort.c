6de53f1337b3f64db4e5db9ffb3eedf7858dcec1
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-19 11:16:15 -0800

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,29 +7,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp, flag;
+	size_t i, j, c;
+	int ma = array[0];
 
-	if (array == NULL || size < 2)
-		return;
-
-	for (i = 0; i < (size - 1); i++)
+	for (i = 0; i < size; i++)
 	{
-		flag = 0;
-		for (j = 0; j < (size - 1 - i); j++)
+		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (ma < array[j])
+				ma = array[j];
+			if (ma > array[j])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				c = array[j];
+				array[j] = ma;
+				array[j - 1] = c;
 				print_array(array, size);
-				flag = 1;
 			}
 		}
-		if (flag == 0)
-		{
-			break;
-		}
+		ma = 0;
 	}
 }
