61f2232014b3e58b57146108632e8ba6711ddbf5
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-21 18:37:51 +0000

    shell

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,29 +8,29 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t j, i, gap = 1;
-    int key;
+	size_t j, i, gap = 1;
+	int key;
 
-    while (gap <= size / 3)
-    {
-        gap = gap * 3 + 1;
-    }
+	while (gap <= size / 3)
+	{
+		gap = gap * 3 + 1;
+	}
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; ++i)
-        {
-            key = array[i];
-            j = i;
-            while (j >= gap && array[j - gap] > key)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
+	while (gap > 0)
+	{
+		for (i = gap; i < size; ++i)
+		{
+			key = array[i];
+			j = i;
+			while (j >= gap && array[j - gap] > key)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
 
-            array[j] = key;   
-        }
-        gap = (gap - 1) / 3;
-        print_array(array, size);
-    }
+			array[j] = key;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
 }
